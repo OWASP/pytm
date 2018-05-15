8a3bab338f515b4cb8446660ec6dfd550dfd29df
Izar Tarandach <izar.tarandach@autodesk.com>
2018-05-14 20:04:22 -0700

    ALmost working draft

--- pytm/pytm.py
+++ pytm/pytm.py
@@ -1,49 +1,86 @@
 class TM():
-    ''' Describes the threat model and contains the bag of flows 
+    ''' Describes the threat model and contains the bag of flows\ 
     and of elements '''
 
-    BagOfFlows = ()
-    BagOfElements = ()
+    BagOfFlows = []
+    BagOfElements = []
 
-    def __init__(self, name, descr):
+    def __init__(self, name, descr=""):
         self.name = name
-        self.descr = descr
+        self.description = descr
+
+    def set_description(self, descr):
+        self.description = descr
 
     def verify(self):
         pass
 
 
-class TMElement():
+class Element():
     counter = 0
 
     def __init__(self, name):
-        counter += 1
+        Element.counter += 1
         self.name = name
         
+    def set_description(self, descr):
+        self.descr = descr
+
     def verify(self):
         ''' makes sure it is good to go '''
         # all minimum annotations are in place
         # then add itself to BagOfElements
-        
+        pass
 
-class TMDataflow():
+
+class Server(Element):
+    pass
+
+
+class Database(Element):
+    pass
+
+
+class Actor(Element):
+    pass
+
+
+class Process(Element):
+    pass
+
+
+class SetOfProcesses(Element):
+    pass
+
+
+class Dataflow():
     counter = 0
 
     def __init__(self, source, sink, name):
-        counter += 1
+        Dataflow.counter += 1
         self.source = source
         self.sink = sink
         self.name = name
+        self.protocol = ""
+        self.authenticatedWith = ""
+        TM.BagOfFlows.append(self)
+
+    def set_source(self, source):
+        self.source = source
+
+    def set_sink(self, sink):
+        self.sink = sink
 
     def verify(self):
         ''' makes sure it is good to go '''
         
         # all minimum annotations are in place
         # then add itself to BagOfFlows
+        pass
             
     @classmethod
     def count(cls):
-        print("{} dataflows are defined.".format(cls.counter))
+        return len(TM.BagOfFlows)
 
 
 
---
+++ pytm/tm.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from pytm import TM, Server, Database, Dataflow
+
+tm = TM("my test tm")
+tm.description = "another test tm"
+
+
+web = Server("web server")
+db = Database("database server")
+
+web_and_db = Dataflow(web, db, "web and db")
+web_and_db.protocol = "HTTP"
+
+print(Dataflow.count())
