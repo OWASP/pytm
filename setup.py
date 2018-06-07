bfd3f74e79d6346a25f835d0a66d36a58dae13a4
Izar Tarandach <izar.tarandach@autodesk.com>
2018-06-07 10:57:33 -0700

    Adding packaging scaffold

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='pytm',
+    version='0.1',
+    packages=['pytm'],
+    long_description=open('README.md').read(),
+    license='GNU General Public License v3.0',
+)
