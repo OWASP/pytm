393e4503da4a8fc29c9e71743640e9a98746cf81
shambho <shambhuni.rohit@gmail.com>
2018-07-09 16:13:17 -0700

    Updated threat IDs and added a few new threats.

--- pytm/threats.py
+++ pytm/threats.py
@@ -2,63 +2,81 @@ from pytm.pytm import Dataflow, Element, Server, Datastore, Process, SetOfProces
 
 ''' Add threats here '''
 Threats = {
-    "DF1": {
+    "AA01": {
         "description": "Dataflow not authenticated",
         "target": Dataflow,
         "condition": "target.authenticatedWith is False",
         "comments": "instead of looking at both sides of a flow, consider the flow authenticated only if both sides authenticate",
     },
-    "SR1": {
+    "HA01": {
         "description": "Server not hardened",
         "target": Server,
         "condition": "target.isHardened is False",
     },
-    "DS1": {
+    "AU01": {
         "description": "Logs created: verify if sensitive data is stored",
         "target": Datastore,
         "condition": "target.storesLogData is True",
     },
-    "DS2": {
+    "AU02": {
         "description": "Potential weak protections for audit data",
         "target": Datastore,
         "condition": "target.storesLogData is True and target._isEncrypted is False",
     },
-    "PR1": {
+    "AC01": {
         "description": "Process Memory Tampered",
         "source": "Generic Process",
         "target": Process,
         "condition": "target.codeType == 'unmanaged'",
     },
-    "DF2": {
+    "AC02": {
         "description": "Replay Attacks",
         "source": "Generic Process",
         "target": Dataflow,
         "condition": "target.implementsCommunicationProtocol is True and target.implementsNonce is False",
     },
-    "DF3": {
+    "CR01": {
         "description": "Collision Attacks",
         "source": "Generic Process",
         "target": Process,
         "condition": "target.implementsCommunicationProtocol is True",
     },
-    "DS3": {
+    "AU03": {
         "description": "Risks from logging",
         "source": "Generic Process",
         "target": Datastore,
         "condition": "target.storesLogData is True",
     },
-    "AUTH1": {
+    "AA02": {
         "description": "Authenticated Data Flow Compromised",
         "source": "Generic Process OR Data Store OR External Interactor",
         "target": (Process, Datastore),
         "condition": "target.providesConfidentiality is False and target.providesIntegrity is False and target.authenticatesSource is True or target.authenticatesDestination is True",
     },
-    "SQL1": {
+    "IN01": {
       "description": "Potential SQL Injection Vulnerability",
       "source": "Generic Process",
       "target": Datastore,
       "condition": "target._isSQL is True",
     },
+    "IN02": {
+      "description": "XML DTD and XSLT Processing",
+      "source": "Generic Process OR Data Store OR External Interactor",
+      "target": Process,
+      "condition": "target._datatype == 'XML'",
+      },
+    "IN03": {
+      "description": "JavaScript Object Notation Processing",
+      "source": "Generic Process OR Data Store OR External Interactor",
+      "target": Process,
+      "condition": "target._datatype == 'JSON'",
+      },
+    "IN04": {
+      "description": "Cross Site Scripting",
+      "source": "Generic Process OR Data Store OR External Interactor",
+      "target": Server,
+      "condition": "target.sanitizesOutput is False and target.sanitizesInput is False",
+      }
 }
 
 '''
