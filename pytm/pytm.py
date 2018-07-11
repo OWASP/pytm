95523defa9888f072bf69a715f99b5633826bf1b
Izar Tarandach <izar.tarandach@autodesk.com>
2018-07-11 16:32:08 -0700

    Support for sequence diagrams.

--- pytm/pytm.py
+++ pytm/pytm.py
@@ -16,7 +16,7 @@ def _debug(msg):
         stderr.write("DEBUG: {}\n".format(msg))
 
 
-def uniq_name(s):
+def _uniq_name(s):
     ''' transform name in a unique(?) string '''
     h = sha224(s.encode('utf-8')).hexdigest()
     return sub(r'[0-9]', '', h)
@@ -115,6 +115,21 @@ class TM():
             e.dfd()
         print("}")
 
+    def seq(self):
+        print("@startuml")
+        for e in TM._BagOfElements:
+            if type(e) is Actor:
+                print("actor {0} as \"{1}\"".format(_uniq_name(e.name), e.name))
+            elif type(e) is Datastore:
+                print("database {0} as \"{1}\"".format(_uniq_name(e.name), e.name))
+            elif type(e) is not Dataflow and type(e) is not Boundary:
+                print("entity {0} as \"{1}\"".format(_uniq_name(e.name), e.name))
+
+        ordered = sorted(TM._BagOfFlows, key=lambda flow: flow.order)
+        for e in ordered:
+            print("{0} -> {1}: {2}".format(_uniq_name(e.source.name), _uniq_name(e.sink.name), e.name)) 
+        print("@enduml")
+
     def report(self, *args, **kwargs):
         print("/* threats = ")
         for f in TM._BagOfFindings:
@@ -127,6 +142,8 @@ class TM():
             _args.report = True
             _args.dfd = True
             _args.resolve = True
+        if _args.seq is True:
+            self.seq()
         if _args.dfd is True:
             self.dfd()
         if _args.resolve is True:
@@ -156,8 +173,8 @@ class Element():
         return "Element\nName: {0}\nTrust Boundary: {1}\nDescription: {2}\n".format(self._name, self._inBoundary.name, self._descr)
  
     def dfd(self):
-        print("{} [".format(uniq_name(self._name)))
-        print('\tshape = circle;\n\tstyle = bold;\n\tlabel = <<table border="0" cellborder="0" cellpadding="2"><tr><td><b>{}</b></td></tr></table>>;\n\t]'.format(uniq_name(self._name)))
+        print("{} [".format(_uniq_name(self._name)))
+        print('\tshape = circle;\n\tstyle = bold;\n\tlabel = <<table border="0" cellborder="0" cellpadding="2"><tr><td><b>{}</b></td></tr></table>>;\n\t]'.format(_uniq_name(self._name)))
 
     @property
     def name(self):
@@ -235,7 +252,7 @@ class Server(Element):
 
     def dfd(self):
         color = _setColor(self)
-        print("{0} [\n\tshape = circle\n\tcolor = {1}".format(uniq_name(self.name), color))
+        print("{0} [\n\tshape = circle\n\tcolor = {1}".format(_uniq_name(self.name), color))
         print('\tlabel = <<table border="0" cellborder="0" cellpadding="2"><tr><td><b>{}</b></td></tr></table>>;'.format(self.name))
         print("]")
 
@@ -260,7 +277,7 @@ class Datastore(Element):
     
     def dfd(self):
         color = _setColor(self)
-        print("{0} [\n\tshape = none;\n\tcolor = {1};".format(uniq_name(self.name), color))
+        print("{0} [\n\tshape = none;\n\tcolor = {1};".format(_uniq_name(self.name), color))
         print('\tlabel = <<table sides="TB" cellborder="0" cellpadding="2"><tr><td><font color="{1}"><b>{0}</b></font></td></tr></table>>;'.format(self.name, color))
         print("]")
     
@@ -357,7 +374,7 @@ class Actor(Element):
         print("Name: {}\nAdmin: {}\nDescription: {}\n".format(self._name, self._isAdmin, self._descr))
 
     def dfd(self):
-        print("%s [\n\tshape = square;" % uniq_name(self._name))
+        print("%s [\n\tshape = square;" % _uniq_name(self._name))
         print('\tlabel = <<table border="0" cellborder="0" cellpadding="2"><tr><td><b>{0}</b></td></tr></table>>;'.format(self._name))
         print("]")
     
@@ -379,7 +396,7 @@ class Process(Element):
         super().__init__(name)
 
     def dfd(self):
-        print("%s [\n\tshape = circle\n" % uniq_name(self._name))
+        print("%s [\n\tshape = circle\n" % _uniq_name(self._name))
         print('\tlabel = <<table border="0" cellborder="0" cellpadding="2"><tr><td><b>{}</b></td></tr></table>>;'.format(self._name))
         print("]")
 
@@ -400,7 +417,7 @@ class SetOfProcesses(Process):
         super().__init__(name)
 
     def dfd(self):
-        print("%s [\n\tshape = doublecircle\n" % uniq_name(self.name))
+        print("%s [\n\tshape = doublecircle\n" % _uniq_name(self.name))
         print('\tlabel = <<table border="0" cellborder="0" cellpadding="2"><tr><td><b>{}</b></td></tr></table>>;'.format(self.name))
         print("]")
 
@@ -517,14 +534,15 @@ class Dataflow(Element):
         pass
 
     def dfd(self):
-        print("\t{0} -> {1} [".format(uniq_name(self._source.name),
-                                      uniq_name(self._sink._name)))
+        print("\t{0} -> {1} [".format(_uniq_name(self._source.name),
+                                      _uniq_name(self._sink._name)))
         color = _setColor(self)
         if self._order >= 0:
             print('\t\tcolor = {2};\n\t\tlabel = <<table border="0" cellborder="0" cellpadding="2"><tr><td><font color="{2}"><b>({0}) {1}</b></font></td></tr></table>>;'.format(self._order, self._name, color))
         else:
             print('\t\tcolor = {1};\n\t\tlabel = <<table border="0" cellborder="0" cellpadding="2"><tr><td><font color ="{1}"><b>{0}</b></font></td></tr></table>>;'.format(self._name, color))
         print("\t]")         
+
 
 class Boundary(Element):
     def __init__(self, name):
@@ -533,7 +551,7 @@ class Boundary(Element):
             TM._BagOfBoundaries.append(self)
 
     def dfd(self):
-        print("subgraph cluster_{0} {{\n\tgraph [\n\t\tfontsize = 10;\n\t\tfontcolor = firebrick2;\n\t\tstyle = dashed;\n\t\tcolor = firebrick2;\n\t\tlabel = <<i>{1}</i>>;\n\t]\n".format(uniq_name(self._name), self._name))
+        print("subgraph cluster_{0} {{\n\tgraph [\n\t\tfontsize = 10;\n\t\tfontcolor = firebrick2;\n\t\tstyle = dashed;\n\t\tcolor = firebrick2;\n\t\tlabel = <<i>{1}</i>>;\n\t]\n".format(_uniq_name(self._name), self._name))
         
         for e in TM._BagOfElements:
             _debug("{0}".format(e.name))
@@ -546,19 +564,23 @@ class Boundary(Element):
         print("\n}\n")
         
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--debug', action='store_true', help='print debug messages')
-parser.add_argument('--resolve', action='store_true', help='identify threats')
-parser.add_argument('--dfd', action='store_true', help='output DFD')
-parser.add_argument('--report', action='store_true', help='output report')
-parser.add_argument('--all', action='store_true', help='output everything')
-parser.add_argument('--exclude', help='specify threat IDs to be ignored')
-_args = parser.parse_args()
-if _args.dfd is False and _args.report is False and _args.resolve is False:
+_parser = argparse.ArgumentParser()
+_parser.add_argument('--debug', action='store_true', help='print debug messages')
+_parser.add_argument('--resolve', action='store_true', help='identify threats')
+_parser.add_argument('--dfd', action='store_true', help='output DFD (default)')
+_parser.add_argument('--report', action='store_true', help='output report')
+_parser.add_argument('--all', action='store_true', help='output everything')
+_parser.add_argument('--exclude', help='specify threat IDs to be ignored')
+_parser.add_argument('--seq', action='store_true', help='output sequential diagram')
+_args = _parser.parse_args()
+if _args.dfd is True and _args.seq is True:
+    print("Cannot produce DFD and sequential diagrams in the same run.")
+    exit(0)
+if _args.dfd is False and _args.report is False and _args.resolve is False and _args.seq is False:
     _args.all = True
 if _args.exclude is not None:
     TM._threatsExcluded = _args.exclude.split(",")
     _debug("Excluding threats: {}".format(TM._threatsExcluded))
     
-    
+
 from pytm.threats import Threats
