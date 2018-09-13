26d55ccf6f6e3b6ce71674956e362d4551dff5e9
Izar Tarandach <izar.tarandach@autodesk.com>
2018-09-13 09:57:03 -0700

    Created and tested setup info for pip (with testpypi)

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+packages=setuptools.find_packages()
+
+setuptools.setup(
     name='pytm',
     version='0.1',
     packages=['pytm'],
     long_description=open('README.md').read(),
     license='MIT License',
+    url="https://github.com/izar/pytm",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+	"Environment :: Console",
+	"Intended Audience :: Developers",
+	"Topic :: Security",
+    ],
 )
