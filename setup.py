75fcd6a1711bd6549bcb2f83a00c7ef241fc03e0
Izar Tarandach <izar.tarandach@autodesk.com>
2018-10-17 13:58:37 -0700

    Version 0.3 deployed to PyPI

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ packages=setuptools.find_packages()
 
 setuptools.setup(
     name='pytm',
-    version='0.1',
+    version='0.3',
     packages=['pytm'],
+    summary='A Python-based framework for threat modeling.',
     long_description=open('README.md').read(),
     license='MIT License',
     url="https://github.com/izar/pytm",
@@ -18,4 +19,5 @@ setuptools.setup(
 	"Intended Audience :: Developers",
 	"Topic :: Security",
     ],
+    python_requires='>=3'
 )
