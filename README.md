aaf9710c9d19eeec0e0a7a3d5372a810b9aa3c08
Izar Tarandach <dev.tarandach.izar@dev.bwater.com>
2021-04-13 17:06:38 -0400

    Bringing samples and code together

--- README.md
+++ README.md
@@ -141,20 +141,20 @@ my_lambda_to_db.dstPort = 3306
 user_to_web = Dataflow(user, web, "User enters comments (*)")
 user_to_web.protocol = "HTTP"
 user_to_web.dstPort = 80
-user_to_web.data = Data(description='Comments in HTML or Markdown', classification=Classification.PUBLIC)
+user_to_web.data = Data('Comments in HTML or Markdown', classification=Classification.PUBLIC)
 
 web_to_user = Dataflow(web, user, "Comments saved (*)")
 web_to_user.protocol = "HTTP"
-web_to_user.data = 'Ack of saving or error message, in JSON'
 
 web_to_db = Dataflow(web, db, "Insert query with comments")
 web_to_db.protocol = "MySQL"
 web_to_db.dstPort = 3306
-web_to_db.data = 'MySQL insert statement, all literals'
-
+# this is a BAD way of defining a data object, here for a demo on how it
+# will appear on the sample report. Use Data objects.
+db_to_web.data = 'Results of insert op'
 db_to_web = Dataflow(db, web, "Comments contents")
 db_to_web.protocol = "MySQL"
-db_to_web.data = 'Results of insert op'
+
 
 tm.process()
 
--- tm.py
+++ tm.py
@@ -56,42 +56,49 @@ my_lambda.hasAccessControl = True
 my_lambda.inBoundary = vpc
 my_lambda.levels = [1, 2]
 
+token_user_identity = Data("Token verifying user identity", classification=Classification.SECRET)
 db_to_secretDb = Dataflow(db, secretDb, "Database verify real user identity")
 db_to_secretDb.protocol = "RDA-TCP"
 db_to_secretDb.dstPort = 40234
-db_to_secretDb.data = "Token to verify user identity"
+db_to_secretDb.data = token_user_identity
 db_to_secretDb.note = "Verifying that the user is who they say they are."
 db_to_secretDb.maxClassification = Classification.SECRET
 
+comments_in_text = Data("Comments in HTML or Markdown", classification=Classification.PUBLIC)
 user_to_web = Dataflow(user, web, "User enters comments (*)")
 user_to_web.protocol = "HTTP"
 user_to_web.dstPort = 80
-user_to_web.data = "Comments in HTML or Markdown"
+user_to_web.data = comments_in_text
 user_to_web.note = "This is a simple web app\nthat stores and retrieves user comments."
 
 web_to_db = Dataflow(web, db, "Insert query with comments")
 web_to_db.protocol = "MySQL"
 web_to_db.dstPort = 3306
+# this is a BAD way of defining a data object, here for a demo on how it
+# will appear on the sample report. Use Data objects.
 web_to_db.data = "MySQL insert statement, all literals"
 web_to_db.note = (
     "Web server inserts user comments\ninto it's SQL query and stores them in the DB."
 )
 
+comment_retrieved = Data("Web server retrieves comments from DB", classification=Classification.PUBLIC)
 db_to_web = Dataflow(db, web, "Retrieve comments")
 db_to_web.protocol = "MySQL"
 db_to_web.dstPort = 80
-db_to_web.data = "Web server retrieves comments from DB"
+db_to_web.data = comment_retrieved
 db_to_web.responseTo = web_to_db
 
+comment_to_show = Data("Web server shows comments to the end user", classifcation=Classification.PUBLIC)
 web_to_user = Dataflow(web, user, "Show comments (*)")
 web_to_user.protocol = "HTTP"
-web_to_user.data = "Web server shows comments to the end user"
+web_to_user.data = comment_to_show
 web_to_user.responseTo = user_to_web
 
-my_lambda_to_db = Dataflow(my_lambda, db, "Lambda periodically cleans DB")
+clear_op = Data("Serverless function clears DB", classification=Classification.PUBLIC)
+my_lambda_to_db = Dataflow(my_lambda, db, "Serverless function periodically cleans DB")
 my_lambda_to_db.protocol = "MySQL"
 my_lambda_to_db.dstPort = 3306
-my_lambda_to_db.data = "Lambda clears DB every 6 hours"
+my_lambda_to_db.data = clear_op
 
 userIdToken = Data(
     name="User ID Token",
